91437ef6a1eebaa7d3a0da95c93cb63d46155dd7
Gudauu <gudautd@gmail.com>
2023-05-19 14:57:30 +0800

    toast_custom & buggy image url(error code ENOENT)

---
+++ app/src/main/java/com/example/wewallhere/Upload/ApiService.java
@@ -0,0 +1,15 @@
+package com.example.wewallhere.Upload;
+
+import okhttp3.MultipartBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
+public interface ApiService {
+    @Multipart
+    @POST("/upload") // Replace with your server's upload endpoint
+    Call<ResponseBody> uploadImage(@Part MultipartBody.Part image);
+}
+
