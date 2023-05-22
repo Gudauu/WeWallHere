471170336dfc37e71ad2c27288a28bb38bab67ff
Gudauu <gudautd@gmail.com>
2023-05-22 23:13:50 +0800

    ExploreListActivity: test on all media

---
+++ app/src/main/java/com/example/wewallhere/ExploreByList/MediaService.java
@@ -0,0 +1,22 @@
+package com.example.wewallhere.ExploreByList;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface MediaService {
+    @GET("media/{type}")
+    Call<List<MediaEntry>> getMediaList(
+            @Path("type") String type
+    );
+}
+
+//public interface MediaService {
+//    @GET("media/{type}/{id}")
+//    Call<List<MediaEntry>> getMediaList(
+//            @Path("type") String type,
+//            @Path("id") String id
+//    );
+//}
