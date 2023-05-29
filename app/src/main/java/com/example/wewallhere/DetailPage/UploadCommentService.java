5a139ccee739abb55c7289217620decc7265e621
Gudauu <gudautd@gmail.com>
2023-05-29 22:47:37 +0800

    retrofit doesn't provide converter for HashMap

--- app/src/main/java/com/example/wewallhere/DetailPage/UploadCommentService.java
+++ app/src/main/java/com/example/wewallhere/DetailPage/UploadCommentService.java
@@ -1,9 +1,12 @@
 package com.example.wewallhere.DetailPage;
 
+import java.util.HashMap;
+
 import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
@@ -29,13 +32,10 @@ public interface UploadCommentService {
             @Part("content") RequestBody content
     );
 
-    @Multipart
     @POST("/uploadComment/text") // Replace with your server's upload endpoint
-    Call<ResponseBody> uploadText(
-            @Part("ID") RequestBody ID,
-            @Part("ID_reply") RequestBody ID_reply,
-            @Part("title") RequestBody title,
-            @Part("content") RequestBody content
-    );
+    Call<Void> uploadText(@Body HashMap<String, String> data);
+
+//    @POST("/data")
+//    Call<Void> sendData(@Body HashMap<String, String> data);
 }
 
